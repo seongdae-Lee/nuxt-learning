d5b10bfa8f2c38cc454fb55091042d30db1fcd4f
lsd94@ubcare.co.kr <lsd94@ubcare.co.kr>
2024-09-03 08:39:56 +0900

    nuxtApp (Runtime App Context)

--- app.vue
+++ app.vue
@@ -3,4 +3,15 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+console.log("nuxtApp : ", nuxtApp);
+
+//vue application instance
+// nuxtApp.vueApp.component()
+// nuxtApp.vueApp.directive()
+// nuxtApp.vueApp.use()
+
+// provide 활용
+// nuxtApp.provide("hello", (msg: string) => `hello ${msg}`);
+</script>
