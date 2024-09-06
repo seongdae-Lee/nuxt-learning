0e47bd4428a097296bee0b1399ab55a1805d1ca9
lsd94@ubcare.co.kr <lsd94@ubcare.co.kr>
2024-09-06 17:22:10 +0900

    Page 공통 컴포넌트 단들기 (쉬어가요)

--- pages/course/prefetching-1.vue
+++ pages/course/prefetching-1.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page padding>
-    <div class="text-center q-py-xl">
-      <div class="text-h4">
-        {{ $route.path }}
-      </div>
-    </div>
+    <div class="text-center q-py-xl"><PageTitle :title="$route.path" /></div>
   </q-page>
 </template>
 <script lang="ts">
