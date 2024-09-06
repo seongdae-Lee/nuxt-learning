0de3782ba4d67fe3d42a28a0e890ca129e3c93f0
lsd94@ubcare.co.kr <lsd94@ubcare.co.kr>
2024-09-06 10:54:21 +0900

    Error Utils - showError

--- plugins/error-handler.ts
+++ plugins/error-handler.ts
@@ -1,6 +1,16 @@
 import { Notify } from "quasar";
 
 export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook("app:error", (err) => {
+    // console.log("vue:error", err);
+    if (err instanceof Error) {
+      Notify.create({
+        message: err.message,
+        type: "warning",
+      });
+    }
+  });
+
   nuxtApp.hook("vue:error", (err) => {
     // console.log("vue:error", err);
     if (err instanceof Error) {
